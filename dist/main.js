cd7820fd505ad8c5e8641fa2b44dd93e297d3280
venkat <bvenkatr2@gmail.com>
2020-05-22 01:16:58 +0530

    understaing deno from zygote

---
+++ dist/main.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var ts = require("typescript");
+V8Worker2.print("Hello World", ts.createProgram);
---
+++ go.mod
@@ -0,0 +1,5 @@
+module deno
+
+go 1.14
+
+require github.com/ry/v8worker2 v0.0.0-20190817054915-735c3ad65d76
